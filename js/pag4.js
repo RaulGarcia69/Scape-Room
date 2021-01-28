b9d30b215dbe714981b2d6b260507a4cfbc8f44c
marcortizgonzalez <marcortizgonzalez2002@gmail.com>
2021-01-28 20:10:37 +0100

    28/01 Marc5.0

---
+++ js/pag4.js
@@ -0,0 +1,9 @@
+function pag4() {
+    var a = document.getElementById('val-1').value
+    if (a == 'MO02') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo Crack!')
+        return false
+    }
+}
