c74ff712501a3c7b329c97a276745c8a6fddfa33
ivnaguinaga <ivan7agui@gmail.com>
2021-01-29 13:04:24 +0100

    dasdas asdma!

---
+++ js/pag7.js
@@ -0,0 +1,9 @@
+function pag7() {
+    var a = document.getElementById('val-1').value
+    if (a == 'QWE123') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo Crack!')
+        return false
+    }
+}
