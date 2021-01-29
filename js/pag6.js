5bd8b576330448e0c47f492c6b9bb28506382e0a
raulgarcia69 <raulgarcia0144@gmail.com>
2021-01-29 17:17:32 +0100

    monke

---
+++ js/pag6.js
@@ -0,0 +1,9 @@
+function pag6() {
+    var a = document.getElementById('val-1').value
+    if (a == 'q75e') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo Crack!')
+        return false
+    }
+}
