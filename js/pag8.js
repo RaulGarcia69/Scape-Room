cf56048cfa9b3a630acb821ab7f1094c08a29e6d
marcortizgonzalez <marcortizgonzalez2002@gmail.com>
2021-02-01 01:20:53 +0100

    Last Marc2.0

--- js/pag8.js
+++ js/pag8.js
@@ -1,6 +1,6 @@
 function pag4() {
     var a = document.getElementById('val-1').value
-    if (a == 'JAMÁS' || a == "jamás") {
+    if (a == 'JAMÁS' || a == "jamás" || a == "Jamás") {
         return true
     } else {
         alert('¡Nooooooo! Le has hecho daño a una mujer')
